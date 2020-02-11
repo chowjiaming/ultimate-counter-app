c20c5b88153b4ce43beaf4dfb28b372e3cec6707
joefstack <joefstack@bitbucket.org>
2020-02-11 13:26:49 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## Ultimate Counter App
---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+
+// Serve client
+app.use(express.static("client/build"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+
+// Server listen
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
